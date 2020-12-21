6b92da446eb99a8aefacd4806918733ec016e4c9
Omri Gazitt <ogazitt@gmail.com>
2020-12-21 14:37:01 -0800

    made loadAuthzMap function require the access token and removed AsertoProvider parameter

--- src/index.js
+++ src/index.js
@@ -4,60 +4,35 @@ import createAsertoClient from '@aserto/aserto-spa-js'
 export const AsertoContext = React.createContext();
 export const useAserto = () => useContext(AsertoContext);
 export const AsertoProvider = ({
-  children,
-  getToken
+  children
 }) => {
   const [asertoClient, setAsertoClient] = useState();
   const [loading, setLoading] = useState(true);
   const [token, setToken] = useState();
   const [authzMap, setAuthzMap] = useState();
 
-  const loadAuthzMap = useCallback(() => {
+  const loadAuthzMap = useCallback((accessToken) => {
     async function load() {
       setLoading(true);
-      const map = await asertoClient.getAuthorizationMap();
+
+      // always refresh the stored access token
+      if (!token || TRUE) {
+        setToken(accessToken);
+      }
+
+      // create a new aserto client
+      const asertoFromHook = await createAsertoClient(accessToken);
+      setAsertoClient(asertoFromHook);
+
+      // retrieve authorization map
+      const map = await asertoFromHook.getAuthorizationMap();
       setAuthzMap(map);
+
       setLoading(false);
     };
 
-    if (!asertoClient) {
-      initAserto();
-    }
-
-    if (asertoClient) {
-      load();
-    }
+    load();
   }, [asertoClient]);
-
-  const initAserto = useCallback(() => {
-    async function createClient() {
-      // if we don't have an access token, try to obtain one
-      let accessToken = token;
-      if (getToken && !accessToken) {
-        accessToken = await getToken();
-        if (accessToken) {
-          setToken(accessToken);
-        } 
-      }
-
-      if (accessToken) {
-        const asertoFromHook = await createAsertoClient(accessToken);
-        setAsertoClient(asertoFromHook);
-        loadAuthzMap();
-      }
-    };
-
-    // if the client hasn't been created yet, create it now
-    if (!asertoClient) {
-      createClient();
-    }
-  }, [getToken, token]);
-/*
-  useEffect(() => {
-    initAserto();
-    // eslint-disable-next-line
-  }, [getToken, token]);
-*/
 
   return (
     <AsertoContext.Provider
